a4be48a98a47f89ea108ba6633c4c26c668eb98c
Bartomeu Miro <bmiro@members.fsf.org>
2011-01-14 14:02:26 +0100

    Arreglant rutines lexiques.

--- decls-crea_arbre.adb
+++ decls-crea_arbre.adb
@@ -175,7 +175,44 @@ package body decls.crea_arbre is
       a.rc_llista_ref:= ref_comp;
    end crea_n_ref_comp;
 
-	procedure remunta_fill(a: out ast; fill: in ast) is
+
+   procedure rl_atom(yylval : out ast; lin, col : in natural) is
+   begin
+      yylval := (at_atom, lin, col);
+   end rl_atom;
+
+   procedure rl_id(yylval : out ast; yytext : in string;
+		   lin, col : in natural) is
+      id : id_nom;
+   begin
+      posa_id(tn, yytext, id);
+      yylval := (at_identificador, lin, col, id);
+   end rl_id;
+
+   procedure rl_lit_enter(yylval : out ast;
+			  lin, col : in natural) is
+   begin
+      yylval := (at_lit_enter, lin, col,
+		 integer(character'pos(yytext(yytext'first))) - 48, enter);
+   end rl_lit_enter;
+
+   procedure rl_lit_string(yylval : out ast;
+			   lin, col : in natural) is
+      id : id_string;
+   begin
+      posa_cad(tn, yytext(yytext'first+1..yytext'last-1), id);
+      yylval := (at_lit_string, lin, col, id, cadena);
+   end rl_lit_string;
+
+   procedure rl_lit_caracter(yylval : out ast;
+			     lin, col : in natural) is
+   begin
+      yylval := (at_lit_caracter, lin, col,
+		 integer(character'pos(yytext(yytext'first+1))), caracter);
+   end rl_lit_caracter;
+
+
+   procedure remunta_fill(a: out ast; fill: in ast) is
    begin
       a:= fill;
    end remunta_fill;
